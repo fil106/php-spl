cc68cded3890b40d39f3652012b4912c0456393b
Filipp Fedorov <fil106@mail.ru>
2018-09-02 13:40:00 +0300

    Chat class

---
+++ Daemon/Chat.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace MyChat;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface
+{
+    protected $clients;
+    public $messages = [];
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        // Store the new connection to send messages to later
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        array_push($this->messages, $from->resourceId . ': ' . $msg);
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        switch ($msg) {
+            case 'messages':
+                echo implode("\n", $this->messages) . "\n";
+            break;
+            case 'last-message':
+                array_pop($this->messages);
+                $from->send($this->messages[count($this->messages)-1]);
+            break;
+        }
+
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        // The connection is closed, remove it, as we can no longer send it messages
+        $this->clients->detach($conn);
+
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "An error has occurred: {$e->getMessage()}\n";
+
+        $conn->close();
+    }
+}
