34cb670d61a6b73b1b48402ae3e7b62b1f69b4ce
Filipp Fedorov <fil106@mail.ru>
2018-09-02 13:40:41 +0300

    Chat server start infinity loop and listen WebSocket

---
+++ Daemon/chat-server.php
@@ -0,0 +1,21 @@
+<?php
+
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use MyChat\Chat;
+
+require dirname(__DIR__) . '\Daemon\vendor\autoload.php';
+
+require_once 'Chat.php';
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
+    8080
+);
+
+$server->run();
